d2ca3e373e416d7cd7ed406730178fd757554839
fdmckie <fdmckie@gmail.com>
2019-09-05 09:00:35 -0400

    MathTwo - Week 1 In-class assignment

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+//      declare data type for variables
+        int mynumber;
+        double myothernumber;
+
+//
+
+
+//      place value into variables
+        myothernumber = 1.7938;
+        mynumber = 2;
+
+//      print values in variables onto console
+
+        System.out.println( mynumber );
+        System.out.println();
+        System.out.println( myothernumber );
+
+    }
+}
